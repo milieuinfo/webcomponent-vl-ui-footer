86826841fd0bae32634be81ef8f3a733e15c4679
Tom Coemans <tom.coemans@gmail.com>
2019-12-27 17:26:35 +0100

    introductie vl-ui-footer

---
+++ vl-footer.src.js
@@ -0,0 +1,63 @@
+import { VlElement, define } from '/node_modules/vl-ui-core/vl-core.js';
+import 'https://prod.widgets.burgerprofiel.vlaanderen.be/api/v1/node_modules/@govflanders/vl-widget-polyfill/dist/index.js';
+
+/**
+ * VlFooter
+ * @class
+ * @classdesc De Vlaanderen footer.
+ * 
+ * @extends VlElement
+ * 
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-footer/releases/latest|Release notes}
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-footer/issues|Issues}
+ * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-footer.html|Demo}
+ * 
+ */
+export class VlFooter extends VlElement(HTMLElement) {
+    constructor() {
+        super();
+        this.__addFooterElement();
+    }
+
+    static get id() {
+        return 'footer';
+    }
+
+    static get footer() {
+        return document.getElementById(VlFooter.id);
+    }
+
+    get _widgetURL() {
+        return 'https://tni.widgets.burgerprofiel.dev-vlaanderen.be/api/v1/widget/' + this._widgetUUID + '/embed';
+    }
+
+    get _widgetUUID() {
+        return {
+            'localhost:8080': '0337f8dc-3266-4e7a-8f4a-95fd65189e5b',
+            'localhost:8081': '0337f8dc-3266-4e7a-8f4a-95fd65189e5b',
+            'localhost:9000': '0337f8dc-3266-4e7a-8f4a-95fd65189e5b'
+        }[window.location.host];
+    }
+
+    getFooterTemplate() {
+        return this._template(`
+            <div id="${VlFooter.id}"></div>
+        `);
+    }
+
+    __addFooterElement() {
+        fetch(this._widgetURL)
+            .then((response) => {
+                return response.text();
+            }).then((code) => this.__executeCode(code)).catch(error => console.error(error));
+    }
+
+    __executeCode(code) {
+        if (!VlFooter.footer) {
+            document.body.appendChild(this.getFooterTemplate());
+        }
+        eval(code.replace(/document\.write\((.*?)\);/, 'document.getElementById("' + VlFooter.id + '").innerHTML = $1;'));
+    }
+}
+
+define('vl-footer', VlFooter);
