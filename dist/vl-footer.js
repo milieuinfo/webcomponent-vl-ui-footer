978956b0b829fe658e0b3dc74ba8a344e60c56d6
Tom Coemans <tom.coemans@gmail.com>
2020-11-17 13:42:56 +0100

    package-lock

--- dist/vl-footer.js
+++ dist/vl-footer.js
@@ -42,6 +42,12 @@ export class VlFooter extends vlElement(HTMLElement) {
     return document.getElementById(VlFooter.id);
   }
 
+  disconnectedCallback() {
+    if (this._observer) {
+      this._observer.disconnect();
+    }
+  }
+
   get _widgetURL() {
     const prefix = this._isDevelopment ? 'https://tni.widgets.burgerprofiel.dev-vlaanderen.be/api/v1/widget' : 'https://prod.widgets.burgerprofiel.vlaanderen.be/api/v1/widget';
     return `${prefix}/${this._widgetUUID}/embed`;
@@ -76,45 +82,21 @@ export class VlFooter extends vlElement(HTMLElement) {
     if (!VlFooter.footer) {
       document.body.appendChild(this.getFooterTemplate());
     }
-    this.__observeFooterElementIsAdded();
+    this._observer = this.__observeFooterElementIsAdded();
     Function(code.replace(/document\.write\((.*?)\);/, 'document.getElementById("' + VlFooter.id + '").innerHTML = $1;'))();
   }
 
   __observeFooterElementIsAdded() {
-    const target = document.querySelector('#' + VlFooter.id);
-    const footerObserver = new MutationObserver((mutations, observer) => this.__footerObserverCallback(mutations, observer));
-    const config = {attributes: false, childList: true, characterData: false};
-    footerObserver.observe(target, config);
-  }
-
-  __footerObserverCallback(mutations, observer) {
-    mutations.forEach((mutation) => {
-      if (mutation.type === 'childList') {
-        if ( this.__footerElementIsToegevoegd(mutation.addedNodes)) {
-          this.dispatchEvent(new CustomEvent(VlFooter.EVENTS.ready));
-          observer.disconnect();
-        }
+    const isFooter = (node) => node.tagName === 'FOOTER' || (node.childNodes && [...node.childNodes].some(isFooter));
+    const observer = new MutationObserver((mutations, observer) => {
+      const nodes = mutations.flatMap((mutation) => [...mutation.addedNodes]);
+      if (nodes.some(isFooter)) {
+        this.dispatchEvent(new CustomEvent(VlFooter.EVENTS.ready));
+        observer.disconnect();
       }
     });
-  }
-
-  __footerElementIsToegevoegd(toegevoegdeNodes) {
-    return this.__eenVanDeNodesBevatElement(toegevoegdeNodes, 'FOOTER');
-  }
-
-  __eenVanDeNodesBevatElement(nodeList, element) {
-    if (nodeList) {
-      for (let i = 0; i< nodeList.length; i++) {
-        if (this.__nodeIsElementOfHeeftElementAlsChild(nodeList.item(i), element)) {
-          return true;
-        }
-      }
-    }
-    return false;
-  }
-
-  __nodeIsElementOfHeeftElementAlsChild(node, element) {
-    return node.tagName === element || this.__eenVanDeNodesBevatElement(node.childNodes, element);
+    observer.observe(VlFooter.footer, {childList: true});
+    return observer;
   }
 }
 
