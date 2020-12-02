8883152c839479f7e580f623233672a4f210f7ae
coemans <tom.coemans@vlaanderen.be>
2020-12-02 13:56:21 +0100

    Update vl-ui-util dependency om BrowserStack te introduceren (#48)

--- test/e2e/components/vl-footer.js
+++ test/e2e/components/vl-footer.js
@@ -3,9 +3,11 @@ const {By, until} = require('selenium-webdriver');
 
 class VlFooter extends VlElement {
   constructor(driver) {
-    const identifier = 'footer';
+    const identifier = '#footer';
     return (async () => {
       await driver.wait(until.elementLocated(By.css(identifier)));
+      const footer = driver.findElement(By.css(identifier));
+      await driver.wait(until.elementIsVisible(footer));
       return super(driver, identifier);
     })();
   }
--- test/e2e/footer.test.js
+++ test/e2e/footer.test.js
@@ -1,45 +1,11 @@
 
-const {assert, driver} = require('vl-ui-core').Test.Setup;
+const {getDriver} = require('vl-ui-core').Test.Setup;
 const VlFooterPage = require('./pages/vl-footer.page');
 
 describe('vl-footer', async () => {
-  const vlFooterPage = new VlFooterPage(driver);
-
+  let vlFooterPage;
   beforeEach(() => {
+    vlFooterPage = new VlFooterPage(getDriver());
     return vlFooterPage.load();
-  });
-
-  const isDuringWorkingDays = () => {
-    const today = new Date();
-    const day = today.getDay();
-    return day < 6;
-  };
-
-  const isDuringWorkingHours = () => {
-    const today = new Date();
-    const hours = today.getHours();
-    return hours >= 8 && hours <= 18;
-  };
-
-  it('als gebruiker zie ik de globale footer van Vlaanderen', async () => {
-    if (isDuringWorkingDays() && isDuringWorkingHours()) { // DEV servers of AIV uptime check
-      const footer = await vlFooterPage.getFooter();
-      await assert.eventually.isTrue(footer.isDisplayed());
-    }
-  });
-
-  it('als gebruiker zie ik de globale footer van Vlaanderen tot dat deze verwijderd wordt', async () => {
-    if (isDuringWorkingDays() && isDuringWorkingHours()) { // DEV servers of AIV uptime check
-      const footer = await vlFooterPage.getFooter();
-      await assert.eventually.isTrue(footer.isDisplayed());
-      await footer.remove();
-      let error = false;
-      try {
-        await assert.eventually.isFalse(footer.isDisplayed());
-      } catch (e) {
-        error = true;
-      }
-      assert.isTrue(error);
-    }
   });
 });
