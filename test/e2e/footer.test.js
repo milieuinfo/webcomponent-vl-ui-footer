1da0a5c6bb6880ae231561c8bde38a9e76ffaa7d
coemans <tom.coemans@zeuzios.be>
2021-06-23 12:12:10 +0200

    disable e2e tests (#61)

--- test/e2e/footer.test.js
+++ test/e2e/footer.test.js
@@ -9,22 +9,4 @@ describe('vl-footer', async () => {
     vlFooterPage = new VlFooterPage(getDriver());
     return vlFooterPage.load();
   });
-
-  it('als gebruiker zie ik de globale footer van Vlaanderen', async () => {
-    const footer = await vlFooterPage.getFooter();
-    await assert.eventually.isTrue(footer.isDisplayed());
-  });
-
-  it('als gebruiker zie ik de globale footer van Vlaanderen tot dat deze verwijderd wordt', async () => {
-    const footer = await vlFooterPage.getFooter();
-    await assert.eventually.isTrue(footer.isDisplayed());
-    await footer.remove();
-    let error = false;
-    try {
-      await assert.eventually.isFalse(footer.isDisplayed());
-    } catch (e) {
-      error = true;
-    }
-    assert.isTrue(error);
-  });
 });
